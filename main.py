27d2841d3a798d4bb8ffbabad1d794bb467869f1
Pavan Kesava Rao <pavank@Pavans-MBP.lan>
2023-06-11 17:48:11 -0400

    Add game loop
    - incorporate pygame event listening system
    - update screen display, framerate and quit event

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-import pygame
+import pygame, sys
 
 pygame.init()
-
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Python Tetris")
-
 clock = pygame.time.Clock()
 
+# colors
+dark_blue = (44, 44, 127)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+    # Drawing
+    screen.fill(dark_blue)
+    pygame.display.update()
+    clock.tick(60)
