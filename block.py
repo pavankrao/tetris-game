b4b70f43ab00cc044bba662501a70d7a7041cc27
Pavan Kesava Rao <pavank@Pavans-MBP.lan>
2023-06-14 21:51:39 -0400

    Define L tetromino and rotation states
    - added block parent class with draw method
    - added blocks child class to define shapes

--- block.py
+++ block.py
@@ -1,4 +1,5 @@
 from colors import Colors
+import pygame
 
 class Block:
     def __init__(self, id):
@@ -8,4 +9,10 @@ class Block:
         self.rotation_state = 0
         self.colors = Colors.get_cell_colors()
 
+    def draw(self, screen):
+        tiles = self.cells[self.rotation_state]
+        for tile in tiles:
+            tile_rect = pygame.Rect(tile.column * self.cell_size + 1, tile.row * self.cell_size + 1,
+            self.cell_size - 1, self.cell_size - 1)
+            pygame.draw.rect(screen, self.colors[self.id], tile_rect)
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from grid import Grid
+from blocks import *
 
 pygame.init()
 screen = pygame.display.set_mode((300, 600))
@@ -11,12 +12,7 @@ dark_blue = (44, 44, 127)
 
 game_grid = Grid()
 
-# test grid color fill
-game_grid.grid[0][0] = 1
-game_grid.grid[3][5] = 4
-game_grid.grid[17][8] = 7
-
-game_grid.print_grid()
+block = LBlock()
 
 while True:
     for event in pygame.event.get():
@@ -26,5 +22,6 @@ while True:
     # Drawing
     screen.fill(dark_blue)
     game_grid.draw_grid(screen)
+    block.draw(screen)
     pygame.display.update()
     clock.tick(60)
